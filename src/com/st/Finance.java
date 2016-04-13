6a5766f1ea1db0d8d2be6d2313a392b85eecd0ba
Rainto96 <rainto96@foxmail.com>
2016-04-13 11:55:23 +0800

    已合并代码，私认为初版完成

--- src/com/st/Finance.java
+++ src/com/st/Finance.java
@@ -11,39 +11,46 @@ import org.json.*;
 
 public class Finance {
 	private JSONObject json;
-	/**输入url,输出url的查询结果
+	/**
+	 * 输入url,输出url的查询结果
+	 * 
 	 * @param urlPath
 	 * @return
 	 * @throws Exception
 	 */
-	private String getJsonString(String urlPath) throws Exception {  
-        URL url = new URL(urlPath);  
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();  
-        connection.connect();  
-        InputStream inputStream = connection.getInputStream();  
-        //对应的字符编码转换  
-        Reader reader = new InputStreamReader(inputStream, "UTF-8");  
-        BufferedReader bufferedReader = new BufferedReader(reader);  
-        String str = null;  
-        StringBuffer sb = new StringBuffer();  
-        while ((str = bufferedReader.readLine()) != null) {  
-            sb.append(str);  
-        }  
-        reader.close();  
-        connection.disconnect();  
-        return sb.toString();  
-    } 
-	/**输入货币种类1和2
-	 * 调用private方法getJsonString
-	 * @param cur1 货币source
-	 * @param cur2 货币to
+	private String getJsonString(String urlPath) throws Exception {
+		URL url = new URL(urlPath);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.connect();
+		InputStream inputStream = connection.getInputStream();
+		// 对应的字符编码转换
+		Reader reader = new InputStreamReader(inputStream, "UTF-8");
+		BufferedReader bufferedReader = new BufferedReader(reader);
+		String str = null;
+		StringBuffer sb = new StringBuffer();
+		while ((str = bufferedReader.readLine()) != null) {
+			sb.append(str);
+		}
+		reader.close();
+		connection.disconnect();
+		return sb.toString();
+	}
+
+	/**
+	 * 输入货币种类1和2 调用private方法getJsonString
+	 * 
+	 * @param cur1
+	 *            货币source
+	 * @param cur2
+	 *            货币to
 	 * @return json格式的字符串数据
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public String getQueryResultInJasonForm(String cur1 , String cur2) throws Exception{
 		String querystr = "http://api.k780.com:88/?app=finance.rate&scur="+cur1+"&tcur="+cur2+"&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4";
 		return getJsonString(querystr);
 	}
+	
 	/**初始化，初始化后才能使用get方法查询
 	 * @param cur1
 	 * @param cur2
@@ -53,6 +60,7 @@ public class Finance {
 	private void init(String cur1, String cur2) throws JSONException, Exception{
 		json=new JSONObject(getQueryResultInJasonForm(cur1, cur2));
 	}
+	
 	/**是否需要更新jason这个对象：1.当jason为null时 2.当jason的货币信息与查询请求不符时
 	 * @param cur1
 	 * @param cur2
@@ -108,4 +116,3 @@ public class Finance {
 		System.out.println(f.getUpdateTime("CAD", "CNY"));
 	}
 }
- 
