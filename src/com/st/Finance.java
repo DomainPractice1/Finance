c798ce7fb54cdf55476310a8eb1a7129049370c7
Rainto96 <rainto96@foxmail.com>
2016-04-19 15:23:15 +0800

    添加了输入的货币字符串检测，如果不在给定的集合中，判断为输入的货币字符串错误

--- src/com/st/Finance.java
+++ src/com/st/Finance.java
@@ -6,10 +6,18 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashSet;
 
 import org.json.*;
 
 public class Finance {
+	String[] curSetString=new String[]{"AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA", "ARS", "AMD", "AWG", "AUD", "AZN", "BSD", "BHD", "BDT", "BBD", "BYR", "BZD", "BMD", "BTN", "BOB", "BAM", "BWP", "BRL", "GBP", "BND", "BGN", "BIF", "XOF", "XAF", "XPF", "KHR", "CAD", "CVE", "KYD", "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "HRK", "CUC", "CUP", "CYP", "CZK", "DKK", "DJF", "DOP", "XCD", "EGP", "SVC", "EEK", "ETB", "EUR", "FKP", "FJD", "GMD", "GEL", "GHS", "GIP", "XAU", "GTQ", "GNF", "GYD", "HTG", "HNL", "HKD", "HUF", "ISK", "INR", "IDR", "IRR", "IQD", "ILS", "JMD", "JPY", "JOD", "KZT", "KES", "KWD", "KGS", "LAK", "LVL", "LBP", "LSL", "LRD", "LYD", "LTL", "MOP", "MKD", "MGA", "MWK", "MYR", "MVR", "MTL", "MRO", "MUR", "MXN", "MDL", "MNT", "MAD", "MZN", "MMK", "ANG", "NAD", "NPR", "NZD", "NIO", "NGN", "KPW", "NOK", "OMR", "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR", "RON", "RUB", "RWF", "WST", "STD", "SAR", "RSD", "SCR", "SLL", "XAG", "SGD", "SKK", "SIT", "SBD", "SOS", "ZAR", "KRW", "LKR", "SHP", "SDG", "SRD", "SZL", "SEK", "CHF", "SYP", "TWD", "TZS", "THB", "TOP", "TTD", "TND", "TRY", "TMM", "USD", "UGX", "UAH", "UYU", "AED", "VUV", "VEB", "VND", "YER", "ZMK", "ZWD"};
+	HashSet<String> curSet=new HashSet<String>();
+	
+	Finance(){
+		for(int i=0;i<curSetString.length;i++) curSet.add(curSetString[i]);
+	}
+	
 	private JSONObject json;
 	/**
 	 * 输入url,输出url的查询结果
@@ -47,6 +55,7 @@ public class Finance {
 	 * @throws Exception
 	 */
 	public String getQueryResultInJasonForm(String cur1 , String cur2) throws Exception{
+		if( !(curSet.contains(cur1) || curSet.contains(cur2)) ) return "货币种类输入错误";
 		String querystr = "http://api.k780.com:88/?app=finance.rate&scur="+cur1+"&tcur="+cur2+"&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4";
 		return getJsonString(querystr);
 	}
@@ -79,6 +88,7 @@ public class Finance {
 	 * @throws Exception
 	 */
 	public String getUpdateTime(String cur1,String cur2) throws JSONException, Exception{
+		if( !(curSet.contains(cur1) || curSet.contains(cur2)) ) return "货币种类输入错误";
 		if(needInit(cur1,cur2)) init(cur1,cur2);
 		return json.getJSONObject("result").getString("update");
 	}
@@ -90,6 +100,7 @@ public class Finance {
 	 * @throws Exception
 	 */
 	public String getRate(String cur1,String cur2) throws JSONException, Exception{
+		if( !(curSet.contains(cur1) || curSet.contains(cur2)) ) return "货币种类输入错误";
 		if(needInit(cur1,cur2)) init(cur1,cur2);
 		return json.getJSONObject("result").getString("rate");
 	}
@@ -100,6 +111,7 @@ public class Finance {
 	 * @throws Exception
 	 */
 	public String getRateBasedOnCNY(String cur1) throws JSONException, Exception{
+		if( !(curSet.contains(cur1)) ) return "货币种类输入错误";
 		if(needInit("CNY",cur1)) init("CNY",cur1);
 		return json.getJSONObject("result").getString("rate");
 	}
@@ -111,6 +123,7 @@ public class Finance {
 	 * @throws Exception
 	 */
 	public String getDescription(String cur1,String cur2) throws JSONException, Exception{
+		if( !(curSet.contains(cur1) || curSet.contains(cur2)) ) return "货币种类输入错误";
 		if(needInit(cur1,cur2)) init(cur1,cur2);
 		return json.getJSONObject("result").getString("ratenm");
 	}
